b388736ab596b99693c88d206af6f2be8ed3a91d
Mrinal Seth <mrinalseth3959@gmail.com>
2021-11-01 02:12:53 +0530

    edu table added

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -7,50 +7,39 @@ const Experience = (props) => {
         .then(window.location='/dashboard')
     }
     const education = props.education.map(edu => {
+        var start = new Date(edu.from).toDateString()
+        var end = ""
+        if(edu.to) {
+            end = new Date(edu.to).toDateString()
+        }
         return(
-            <tr key={edu._id}>
-            <td>{edu.school}</td>
-            <td>{edu.degree}</td>
-            <td>{edu.fieldofstudy}</td>
-            <td>{edu.from} - {edu.to}</td>
-            <td>
-                <button className='btn btn-danger' onClick={() => {
-                    handleClick(edu._id)
-                }} >Delete</button>
-            </td>
-        </tr>
+            <div class="table-row">		
+				<div class="table-data">{edu.school}</div>
+				<div class="table-data">{edu.degree}</div>
+				<div class="table-data">{edu.fieldofstudy}</div>
+				<div class="table-data">{start} - {end}</div>
+                <div className="table-data">
+                    <button className='btn btn-danger' onClick={() => {
+                            handleClick(edu._id)
+                            }} >Delete
+                    </button>
+                </div>
+			</div>
         )
     })
     return(
-        <div>
-            <h4 className="mb-4">Education</h4>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>School</th>
-                        <th>Degree</th>
-                        <th>Field</th>
-                        <th>Year</th>
-                        <th></th>
-                    </tr>
-                    {education}
-                </thead>
-            </table>
-        </div>
+        <div class="table">
+            <div class="table-header">
+                <div class="header__item"><a id="name" class="filter__link" href="#">School</a></div>
+                <div class="header__item"><a id="wins" class="filter__link filter__link--number" href="#">Degree</a></div>
+                <div class="header__item"><a id="draws" class="filter__link filter__link--number" href="#">Field</a></div>
+                <div class="header__item"><a id="draws" class="filter__link filter__link--number" href="#">Year</a></div>
+                <div class="header__item"><a id="draws" class="filter__link filter__link--number" href="#"></a></div>
+            </div>
+            <div class="table-content">	
+                {education}
+            </div>	
+	    </div>
     )
 }
-
-// const eduerience = (props) => {
-//     const content = props.eduerience.map((edu) => {
-//         return(
-//             <p>{edu._id}</p>
-//         )
-//     })
-//     return(
-//         <div>
-//             {content}
-//         </div>
-//     )
-// }
-
 export default Experience
