555afbacab220a1cf9ccae0c963918080f9d09a8
Mrinal Seth <mrinalseth3959@gmail.com>
2021-11-01 21:27:12 +0530

    profile completed

--- client/src/components/profile/ProfileCred.js
+++ client/src/components/profile/ProfileCred.js
@@ -1,47 +1,63 @@
 import React from 'react'
+import './ProfileCred.css'
 
 const ProfileCred = (props) =>{
+    // <li key={education._id} className="list-group-item">
+    //                         <h4>{education.school}</h4>
+    //                         <p>{education.from}</p>
+    //                         <p>
+    //                             <strong>Degree: </strong>{education.degree}
+    //                         </p>
+    //                         <p>
+    //                             <strong>Field Of Study: </strong>{education.fieldofstudy}
+    //                         </p>
+    //                             <p>
+    //                         <strong>Description:</strong> {education.description}</p>
+    //                     </li>
     return (
-        <div className="row">
-            <div className="col-md-6">
-              <h3 className="text-center text-info"><i class="fas fa-briefcase"></i>Experience</h3>
-              <ul className="list-group">
+        <div className="container">
+              <h3 className="heading">Experience</h3>
+                <div className="expContainer">
                 {props.profile.experience.map((exp) => {
+                    var start = new Date(exp.from).toLocaleDateString()
+                    var end = "Current"
+                    if(exp.to) {
+                        end = new Date(exp.to).toLocaleDateString()
+                    }
                     return(
-                        <li key={exp._id} className="list-group-item">
-                            <h4>{exp.company}</h4>
-                            <p>{exp.from}</p>
-                            <p>
-                                <strong>Position:</strong> {exp.title}
-                            </p>
-                            <p>
-                                <strong>Description: </strong>{exp.description}</p>
-                        </li>
+                        <div key={exp} className="expCard" data-tilt data-tilt-scale="0.95" data-tilt-startY="40">
+                            <div class="year">
+                                <div><span>Comapny: </span>{exp.company}</div>
+                                <div><span>Year: </span>{start} - {end}</div>
+                                <div><span>Position: </span>{exp.title}</div>
+                                <div><span>Description: </span>{exp.description}</div>
+                            </div>  
+                        </div>
                     )
                 })}
-              </ul>
-            </div>
-            <div className="col-md-6">
-              <h3 className="text-center text-info"><i class="fas fa-graduation-cap"></i>Education</h3>
-              <ul className="list-group">
-                {props.profile.education.map((education) => {
+                </div>
+            <h3 className="heading">Experience</h3>
+
+              <div className="expContainer">
+                {props.profile.education.map((edu) => {
+                    var start = new Date(edu.from).toLocaleDateString()
+                    var end = "Current"
+                    if(edu.to) {
+                        end = new Date(edu.to).toLocaleDateString()
+                    }
                     return(
-                        <li key={education._id} className="list-group-item">
-                            <h4>{education.school}</h4>
-                            <p>{education.from}</p>
-                            <p>
-                                <strong>Degree: </strong>{education.degree}
-                            </p>
-                            <p>
-                                <strong>Field Of Study: </strong>{education.fieldofstudy}
-                            </p>
-                                <p>
-                            <strong>Description:</strong> {education.description}</p>
-                        </li>
+                        <div key={edu} className="expCard" data-tilt data-tilt-scale="0.95" data-tilt-startY="40">
+                            <div class="year">
+                                <div><span>School: </span>{edu.school}</div>
+                                <div><span>Year: </span>{start} - {end}</div>
+                                <div><span>Degree: </span>{edu.degree}</div>
+                                <div><span>Field: </span>{edu.fieldofstudy}</div>
+                                <div><span>Description: </span>{edu.description}</div>
+                            </div>  
+                        </div>
                     )
                 })}
-              </ul>
-            </div>
+              </div>
           </div>
     )
 }
