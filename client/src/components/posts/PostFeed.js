1306fe5a1dfdd624627229e8530abc5a2ced5130
Mrinal Seth <mrinalseth3959@gmail.com>
2021-11-06 18:51:55 +0530

    post feed completed

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 import {deletePost, addLike, removeLike} from '../../actions/postActions'
 import classnames from 'classnames'
+import './PostFeed.css'
 
 const PostFeed = (props) => {
     const dispatch = useDispatch()
@@ -19,31 +20,8 @@ const PostFeed = (props) => {
             return false
         }
     }
-    
-    return (
-        <div >
-        {props.posts.map((post) => {
-            let mediaData
-            if(post.mediaType === 'image'){
-                mediaData = <img src={post.mediaLink} style={{
-                    width: "250px",
-                    height: "250px"
-                }} alt="" />
-            }
-            else if(post.mediaType === 'pdf'){
-                mediaData = <a target="_blank" href={post.mediaLink}>Open PDF</a>
-            }
-            else if(post.mediaType === 'word'){
-                mediaData = <a target="_blank" href={post.mediaLink}>Download word file</a>
-            }
-            else if(post.mediaType === 'audio'){
-                mediaData = <audio controls src={post.mediaLink}></audio>
-            }
-            else if(post.mediaType === 'video'){
-                mediaData = <video controls height="250px" width="500px" src={post.mediaLink}></video>
-            }
-            return(
-                <div  key={post._id}>
+
+                    {/* <div  key={post._id}>
                     <div >
                         <div>
                         <a href="profile.html">
@@ -86,7 +64,75 @@ const PostFeed = (props) => {
                         }
                         </div>
                     </div>
-        </div>
+        </div> */}
+    
+    return (
+        <div className="postFeed" >
+        {props.posts.map((post) => {
+            let mediaData
+            if(post.mediaType === 'image'){
+                mediaData = <img src={post.mediaLink} alt="" />
+            }
+            else if(post.mediaType === 'pdf'){
+                mediaData = <a style={{textDecoration:"none"}} target="_blank" href={post.mediaLink}>
+                    <img src="https://bit.ly/3kectjI" alt="" />
+                    Open PDF
+                </a>
+            }
+            else if(post.mediaType === 'word'){
+                mediaData = <a style={{textDecoration:"none"}} target="_blank" href={post.mediaLink}>
+                    <img src="https://bit.ly/3bJJAHm" alt="" />
+                    Download attached file
+                </a>
+            }
+            else if(post.mediaType === 'audio'){
+                mediaData = <audio controls src={post.mediaLink}></audio>
+            }
+            else if(post.mediaType === 'video'){
+                mediaData = <video controls height="250px" width="500px" src={post.mediaLink}></video>
+            }
+            const date = new Date(post.date)
+            return(
+                <div className="postFeedCard">
+                    <div className="header">
+                        <img src={post.avatar} alt="" />
+                        <div className="details">
+                            <div className="name">{post.name}</div>
+                            <div className="date">{date.toDateString()}</div>
+                        </div>
+                    </div>
+                    <div className="text">
+                        {post.text}
+                    </div>
+                    <div className="media">{mediaData}</div>
+                    <div 
+                        className="reaction"
+                        >
+                        <img
+                        onClick={() => {
+                                dispatch(addLike(post._id))
+                            }}  className="like" src="https://bit.ly/2ZZAL9z" alt="" />
+                        <p className="count">{post.likes.length}</p>
+                        {
+                            findUser(post.likes) ?
+                            <img  onClick={() => {
+                                dispatch(removeLike(post._id))
+                            }} className="unLike" src="https://bit.ly/3BQykU4" alt="" /> :null
+                        }
+                    </div>
+                    <div className="action">
+                    {
+                            post.user === id
+                                ?<button 
+                                type="button"
+                                onClick={() => {onDelete(post._id)}}
+                                >
+                                    <i>Delete</i>
+                                </button>
+                                :null
+                        }
+                    </div>
+                </div>
             )
         })}
 
