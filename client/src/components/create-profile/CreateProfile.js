4447911a1d902fc6f1c2cfc4b844602020320482
Mrinal Seth <mrinalseth3959@gmail.com>
2021-11-07 23:36:48 +0530

    Create profile fixed

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -82,9 +82,6 @@ const CreateProfile = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <a href="dashboard.html" className="btn btn-light">
-                Go Back
-              </a>
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">Let's get some information to make your profile stand out</p>
               <small className="d-block pb-3">* = required field</small>
